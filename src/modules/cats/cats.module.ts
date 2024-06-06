fe4b6fc7ec673827a984af8147c0953d1789e7b5
tung_le_thanh <tung_le_thanh@bita.jp>
2024-06-06 14:07:11 +0700

    feat: apply middleware

--- src/modules/cats/cats.module.ts
+++ src/modules/cats/cats.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { CatsController } from 'src/controllers/cats/cats.controller';
+import { LoggerMiddleware } from 'src/middlewares/logger.middleware';
 import { CatsService } from 'src/services/cats/cats.service';
 @Module({
   controllers: [CatsController],
   providers: [CatsService],
   exports: [CatsService],
 })
-export class CatsModule {}
+export class CatsModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({
+      path: '/cats',
+      method: RequestMethod.GET,
+    });
+  }
+}
