3d76144b0c2e68d2524b1bc15f3c94fcbf56bd8b
tung_le_thanh <tung_le_thanh@bita.jp>
2024-06-06 11:54:14 +0700

    feat:update

--- src/modules/cats/cats.module.ts
+++ src/modules/cats/cats.module.ts
@@ -1,11 +1,8 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CatsController } from 'src/controllers/cats/cats.controller';
-import { UsersController } from 'src/controllers/users/users.controller';
 import { CatsService } from 'src/services/cats/cats.service';
-import { UsersService } from 'src/services/users/users.service';
-@Global()
 @Module({
-  controllers: [CatsController, UsersController],
-  providers: [CatsService, UsersService],
+  controllers: [CatsController],
+  providers: [CatsService],
 })
 export class CatsModule {}
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CatsModule } from '../cats/cats.module';
+import { UsersController } from 'src/controllers/users/users.controller';
+import { UsersService } from 'src/services/users/users.service';
 
 @Module({
-  imports: [CatsModule],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class UsersModule {}
