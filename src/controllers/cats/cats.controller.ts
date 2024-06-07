101bae7385ebae17ab533c06bb19ece33e45b1e7
tung_le_thanh <tung_le_thanh@bita.jp>
2024-06-07 16:53:01 +0700

    feat: guard

--- src/controllers/cats/cats.controller.ts
+++ src/controllers/cats/cats.controller.ts
@@ -8,21 +8,26 @@ import {
   Post,
   Query,
   UseFilters,
+  UseGuards,
   UsePipes,
 } from '@nestjs/common';
 import { CreateCatDto, createCatSchema } from 'src/@types/dto/create-cat.dto';
 import { HttpExceptionFilter } from 'src/exceptions/http-exception.filter';
+import { Roles } from 'src/guard/roles.decorator';
+import { RolesGuard } from 'src/guard/roles.guard';
 import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe';
 import { CatsService } from 'src/services/cats/cats.service';
 import { v4 as uuidv4 } from 'uuid';
 
 @Controller('cats')
 @UseFilters(new HttpExceptionFilter())
+@UseGuards(RolesGuard) // or @UseGuards(new RolesGuard())
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
   @UsePipes(new ZodValidationPipe(createCatSchema))
+  @Roles(['admin'])
   async createCat(@Body() createCatDto: CreateCatDto) {
     this.catsService.createCat({
       id: uuidv4(),
