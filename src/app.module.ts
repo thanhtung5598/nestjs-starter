04c9d09e87f4467bb5d005ddde7f16c3db60b1f2
tung_le_thanh <tung_le_thanh@bita.jp>
2024-06-09 23:19:32 +0700

    feat: intergrate api and docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { CatsModule } from './modules/cats/cats.module';
 import { UsersModule } from './modules/users/users.module';
 import { LoggerModule } from './modules/logger/logger.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [LoggerModule, CatsModule, UsersModule],
+  imports: [
+    LoggerModule,
+    CatsModule,
+    UsersModule,
+    PrismaModule,
+    ArticlesModule,
+  ],
 })
 export class AppModule {}
