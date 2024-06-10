7541a3ba9454572f1c82a784568841a3e1ab750b
tung_le_thanh <tung_le_thanh@bita.jp>
2024-06-10 16:23:31 +0700

    feat: testing

--- src/controllers/cats/cats.controller.spec.ts
+++ src/controllers/cats/cats.controller.spec.ts
@@ -1,18 +1,27 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { CatsController } from './cats.controller';
+import { CatsService } from 'src/services/cats/cats.service';
 
 describe('CatsController', () => {
-  let controller: CatsController;
+  let catsController: CatsController;
+  let catsService: CatsService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef = await Test.createTestingModule({
       controllers: [CatsController],
+      providers: [CatsService],
     }).compile();
 
-    controller = module.get<CatsController>(CatsController);
+    catsService = moduleRef.get<CatsService>(CatsService);
+    catsController = moduleRef.get<CatsController>(CatsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of cats', async () => {
+      const result = [];
+      jest.spyOn(catsService, 'findAllCats').mockImplementation(() => result);
+
+      expect(await catsController.getAllCats()).toBe(result);
+    });
   });
 });
--- src/services/cats/cats.service.spec.ts
+++ src/services/cats/cats.service.spec.ts
@@ -1,18 +1,32 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+import { CatsController } from 'src/controllers/cats/cats.controller';
 import { CatsService } from './cats.service';
 
-describe('CatsService', () => {
-  let service: CatsService;
+const moduleMocker = new ModuleMocker(global);
+
+describe('CatsController', () => {
+  let controller: CatsController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CatsService],
-    }).compile();
+    const moduleRef = await Test.createTestingModule({
+      controllers: [CatsController],
+    })
+      .useMocker((token) => {
+        const results = ['test1', 'test2'];
+        if (token === CatsService) {
+          return { findAll: jest.fn().mockResolvedValue(results) };
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
-    service = module.get<CatsService>(CatsService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+    controller = moduleRef.get(CatsController);
   });
 });
