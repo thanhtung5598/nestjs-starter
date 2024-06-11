4c37e3ee310281a0a43818d2b776afe8a4e49d8e
tung_le_thanh <tung_le_thanh@bita.jp>
2024-06-11 15:39:08 +0700

    feat: emit event

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,27 @@ import { APP_INTERCEPTOR, ModuleRef } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { ScheduleModule } from '@nestjs/schedule';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
+    EventEmitterModule.forRoot({
+      // set this to `true` to use wildcards (ký tự đại diện)
+      wildcard: false,
+      // the delimiter used to segment namespaces
+      delimiter: '.',
+      // set this to `true` if you want to emit the newListener event
+      newListener: false,
+      // set this to `true` if you want to emit the removeListener event
+      removeListener: false,
+      // the maximum amount of listeners that can be assigned to an event
+      maxListeners: 10,
+      // show event name in memory leak message when more than maximum amount of listeners is assigned
+      verboseMemoryLeak: false,
+      // disable throwing uncaughtException if an error event is emitted and it has no listeners
+      ignoreErrors: false,
+    }),
     ScheduleModule.forRoot(),
     CacheModule.register({
       ttl: 5, // seconds
@@ -21,6 +39,7 @@ import { ScheduleModule } from '@nestjs/schedule';
     UsersModule,
     PrismaModule,
     ArticlesModule,
+    OrdersModule,
   ],
   providers: [
     {
